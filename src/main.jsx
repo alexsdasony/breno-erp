94ade41717e1a8bf5026ef1cced95307bb642699
Alexs Souza <indiotaon@gmail.com>
2025-06-28 07:45:28 -0400

    fix: Usa versao API em vez de localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
-import { AppDataProvider } from '@/hooks/useAppData.jsx';
+import { AppDataProvider } from '@/hooks/useAppData.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
